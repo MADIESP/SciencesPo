d3ea65430ac38661773c9bd7d01380932e52f25e
Pauline Madies <pauline.madies@sciencespo.fr>
2024-03-04 16:24:01 +0100

    First commit

---
+++ public_goods_simple/__init__.py
@@ -0,0 +1,70 @@
+from otree.api import *
+
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'public_goods_simple'
+    PLAYERS_PER_GROUP = 4
+    NUM_ROUNDS = 4
+    ENDOWMENT = cu(100)
+    MULTIPLIER = 2
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+
+class Player(BasePlayer):
+    contribution = models.CurrencyField(
+        min=0, max=C.ENDOWMENT, label="How much of your 100 points do you want to contribute?"
+    )
+
+
+# FUNCTIONS
+def set_payoffs(group: Group):
+    players = group.get_players()
+    contributions = [p.contribution for p in players]
+    group.total_contribution = sum(contributions)
+    group.individual_share = (
+        group.total_contribution * C.MULTIPLIER / C.PLAYERS_PER_GROUP
+    )
+    for p in players:
+        p.payoff = C.ENDOWMENT - p.contribution + group.individual_share
+
+# PAGES
+class Instruction(Page):
+    form_model = 'player'
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1
+
+
+class Contribute(Page):
+    form_model = 'player'
+    form_fields = ['contribution']
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = set_payoffs
+
+
+class Results(Page):
+    pass
+
+class WaitforNext(WaitPage):
+    wait_for_all_groups = True
+
+
+class End(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 4
+
+
+page_sequence = [Instruction, Contribute, ResultsWaitPage, Results, WaitforNext, End]
